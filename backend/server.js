c7c69506ce69aa4f4f2c9c67fe98b32530be7668
ArunRupan <arunrupan.contact@gmail.com>
2023-07-11 10:58:05 +0530

    made changes to deploy

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send(`Server ready`);
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+//   );
+// } else {
+// }
+
+app.get("/", (req, res) => {
+  res.send(`Server ready`);
+});
 
 app.use(notFound);
 app.use(errorHandler);
