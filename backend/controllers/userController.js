651876732b78b010cabfe7a22b48992c7e7b0bf7
ArunRupan <arunrupan.contact@gmail.com>
2023-07-12 20:35:37 +0530

    fixing status code 500

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,7 +41,7 @@ const regUser = asyncHandler(async (req, res) => {
   const user = await User.create({ name, email, password });
 
   if (user) {
-    // generateToken(res, user._id);
+    generateToken(res, user._id);
 
     res.status(201).json({
       _id: user._id,
