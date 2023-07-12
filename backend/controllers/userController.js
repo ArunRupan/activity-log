32090de6110bd213ce126acb94c8897cbeae8a88
ArunRupan <arunrupan.contact@gmail.com>
2023-07-12 20:28:53 +0530

    fixing status code 500

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,29 +29,29 @@ const authUser = asyncHandler(async (req, res) => {
 // @access  Public
 const regUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "hello" });
-  // const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-  // const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ email });
 
-  // if (userExists) {
-  //   res.status(400);
-  //   throw new Error("User already exists");
-  // }
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-  // const user = await User.create({ name, email, password });
+  const user = await User.create({ name, email, password });
 
-  // if (user) {
-  //   generateToken(res, user._id);
+  if (user) {
+    // generateToken(res, user._id);
 
-  //   res.status(201).json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //   });
-  // } else {
-  //   res.status(400);
-  //   throw new Error("Invalid user data");
-  // }
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc    Logout user / clear cookie
