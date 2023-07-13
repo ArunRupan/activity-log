9f5ce8c49959b3d4e352b33876b12aa108327523
ArunRupan <arunrupan.contact@gmail.com>
2023-07-13 20:54:00 +0530

    fixing cookie generation on browser

--- backend/utils/genToken.js
+++ backend/utils/genToken.js
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const generateToken = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
+const generateToken = (res, user) => {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN, {
     expiresIn: "30d",
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "none", // Prevent CSRF attacks
+    // secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    // sameSite: "none", // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
