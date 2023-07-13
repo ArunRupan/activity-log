246e2cd837ec09df0724214dc9f46383ce4172d9
ArunRupan <arunrupan.contact@gmail.com>
2023-07-13 20:48:40 +0530

    fixing cookie generation on browser

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const app = express();
 
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/utils/genToken.js
+++ backend/utils/genToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
     expiresIn: "30d",
   });
 
-  res.cookies("jwt", token, {
+  res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
     sameSite: "none", // Prevent CSRF attacks
